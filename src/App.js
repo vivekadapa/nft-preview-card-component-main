095d8ea480d2ba8723827255c236dc242a23b96d
vivekchaitanya <vivekchaitanya1324@gmail.com>
2023-08-15 11:41:51 +0530

    challenge completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="card">
+          <div className='img-container'>
+            <div className="bg-hover"></div>
+            <img src="/images/image-equilibrium.jpg" alt="" className="img-eqb" />
+            <img src="/images/icon-view.svg" alt="" className='eye-icon' />
+          </div>
+           
+            <h3>Equilibrium #3429</h3>
+            <p className='text'>Our Equilibrium collection promotes balance and calm.</p>
+            <div className="flex-container">
+              <div className="flex-child">
+                <img src="/images/icon-ethereum.svg" alt="" />
+                <p>0.041 ETH</p>
+              </div>
+              <div className="flex-child">
+                <img src="/images/icon-clock.svg" alt="" />
+                <p style={{color:"hsla(215, 52%, 70%, 0.683)"}}>3 days left</p>
+              </div>
+            </div>
+            <hr />
+            <div className="flex-foot">
+              <img src="/images/image-avatar.png" alt="" />
+              <p>Creation of <span>Jules Wyvern</span></p>
+            </div>
+        </div>
+    </div>  
   );
 }
 
